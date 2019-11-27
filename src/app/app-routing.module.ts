b083c3015afc73774d8eb0e87a30e43584cb1518
Devin Thomas <dthom94@gmail.com>
2019-11-26 23:31:05 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {AboutComponent} from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { EventComponent } from './event/event.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:MainComponent},
+  {path:'about',component:AboutComponent},
+  {path:'service',component:ServiceComponent},
+  {path:'event', component:EventComponent},
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { EventComponent } from './event/event.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    ServiceComponent,
+    EventComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
